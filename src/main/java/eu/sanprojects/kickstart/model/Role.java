2dc6ff38df5f9135c12edcd32d7f647b531fba60
baldarn <lorenzo.farnararo@gmail.com>
2014-12-03 14:48:17 +0100

    added model 'role' and edited model 'user' for spring security

---
+++ src/main/java/eu/sanprojects/kickstart/model/Role.java
@@ -0,0 +1,52 @@
+package eu.sanprojects.kickstart.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "system_roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	Long id;
+	
+	@NotNull
+	String authority;
+	
+	@ManyToMany(mappedBy = "roles")
+	Set<User> users = new HashSet<User>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	
+}
--- src/main/java/eu/sanprojects/kickstart/model/User.java
+++ src/main/java/eu/sanprojects/kickstart/model/User.java
@@ -1,9 +1,15 @@
 package eu.sanprojects.kickstart.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -16,29 +22,57 @@ public class User {
 	Long id;
 	
 	@NotNull
+	String username;
+	
+	@NotNull
 	String name;
 	
 	@NotNull
 	String description;
-		
+	
+	@ManyToMany
+	@JoinTable(name="system_user_system_role",
+		joinColumns={@JoinColumn(name="user_id", referencedColumnName="id")},
+		inverseJoinColumns={@JoinColumn(name="role_id", referencedColumnName="id")})
+	Set<Role> roles = new HashSet<Role>();
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 }
