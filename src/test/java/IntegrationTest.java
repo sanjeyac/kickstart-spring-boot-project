97ee86ab433968148914380158436e4b0a5089a6
Sanjeya	 Cooray <sanjeya.cooray@gmail.com>
2014-12-12 14:01:17 +0100

    Tools for testing

---
+++ src/test/java/IntegrationTest.java
@@ -0,0 +1,27 @@
+import eu.sanprojects.kickstart.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@org.springframework.boot.test.IntegrationTest("server.port:0")
+public class IntegrationTest {
+
+    @Value("${local.server.port}")
+    int port;
+
+    @Before
+    public void setUp() {
+        System.out.println("Tests setup");
+    }
+
+    @Test
+    public void sampleTest() {
+        System.out.println("Tests sample");
+    }
+
+
+}
